29c8680734a1a1f7ea3495297c18b7dfdc671806
Kleber Luan <kleberluanbleodoro@gmail.com>
2026-03-11 19:58:07 -0300

    Add comments

--- aventureiros/create.php
+++ aventureiros/create.php
@@ -1,1 +1,2 @@
-
+//Em decorrência da ausência de ferramentas no computador da faculdade para que eu possa iniciar meu projeto crud
+//Vou só fazer um arquivo base pra deixar registrado como quero arquitetar a organização de pastas através de dominios

92916c36ba18de77cab736590e0f02b481ec745f
Kleber <kleberluanbleodoro@gmail.com>
2026-03-17 17:27:08 -0300

    delete testing

--- aventureiros/Aventureiro.php
+++ aventureiros/Aventureiro.php
@@ -1,6 +1,10 @@
 <?php
+
+//Criação da classe aventureiro
 class Aventureiro
 {
+
+  //? serve para dizer que a variável pode ser null
   private ?int $id;
   private string $nome;
   private int $classeId;
@@ -8,6 +12,8 @@ class Aventureiro
   private string $status;
   private ?DateTime $dataCriacao;
 
+
+  //Construtor da classe
   public function __construct($nome, $classeId, $exp, $status = 'Y', $id = null, $dataCriacao = null)
   {
     $this->id = $id;
@@ -18,30 +24,41 @@ class Aventureiro
     $this->dataCriacao = $dataCriacao;
   }
 
+
+  //Puxa os dados deste aventureiro do banco de dados 
   public function select()
   {
+    //Primeiro um try catch para lidar com possíveis erros de conexão ou consulta ao banco de dados
     try {
-      $server =  new PDO('mysql:host=localhost:3306;dbname=rpg_crud', 'root', '');
+      //Criando uma conexão com o banco de dados usando PDO, root é o usuário e '' é a senha
+      $server =  new PDO('mysql:host=localhost:3306;dbname=rpg_crud', 'root', 'toor');
+      //Definindo o modo de erro do PDO para exceção, não sei ainda ao certo como funciona
       $server->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+      //Consulta SQL a moda harcoded apenas para testar, depois tem que ser algo mais dinâmico
       $data = ($server->query('SELECT * FROM aventureiros WHERE nome = ' . $server->quote($this->nome)))->fetchAll(PDO::FETCH_ASSOC);
 
       // $data->fetchAll(PDO::FETCH_ASSOC);
 
       #var_dump($data);
+      //Retorna os dados do aventureiro como um array associativo só pra testar
       return $data;
     } catch (PDOException $e) {
+      //Pra exibir erro
       echo 'ERROR: ' . $e->getMessage();
     }
   }
 
+  //Insere os dados deste aventureiro no banco de dados
   public function insert()
   {
     try {
-      $server =  new PDO('mysql:host=localhost:3306;dbname=rpg_crud', 'root', '');
+      $server =  new PDO('mysql:host=localhost:3306;dbname=rpg_crud', 'root', 'toor');
       $server->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+      //Prepared stmt pra previnir injection. O prepare é tipo um template da query, e o execute é onde a gente passa os valores pra preencher esse template
       $stmt = $server->prepare('INSERT INTO aventureiros (nome, classe_id, exp, status) VALUES (:nome, :classeId, :exp, :status)');
+      //Passando parâmetros para o execute, usando os atributos do objeto atual ($this)
       $stmt->execute([
         ':nome' => $this->nome,
         ':classeId' => $this->classeId,
@@ -53,15 +70,18 @@ class Aventureiro
     }
   }
 
+  //Atualiza os dados deste aventureiro no banco de dados
   public function update($nnome)
   {
     try {
-      $server = new PDO('mysql:host=localhost:3306;dbname=rpg_crud', 'root', '');
+      $server = new PDO('mysql:host=localhost:3306;dbname=rpg_crud', 'root', 'toor');
       $server->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+      //Aqui se aplica o mesmmo conceito só que pra update, o id tá hardcoded só pra testar, depois tem que ser algo mais pampa
       $stmt = $server->prepare('UPDATE aventureiros SET nome = :nome where id = :id');
       $stmt->execute([
         ':nome' => $nnome,
+        //id no pelo
         ':id' => 4
       ]);
 
@@ -69,12 +89,32 @@ class Aventureiro
       echo 'Erro' . $e->getMessage();
     }
   }
+
+  public function delete(){
+    try{
+      $server = new PDO('mysql:host=localhost:3306;dbname=rpg_crud', 'root', 'toor');
+      $server->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+      $stmt = $server->prepare('DELETE FROM aventureiros WHERE id = :id');
+      $stmt->execute([
+        ':id' => 4
+      ]);
+    }catch(PDOException $e){
+      echo 'Erro' . $e->getMessage();
+    }
+
+  }
 }
 
-$Carlos = new Aventureiro("Carlos", 4, 100);
-$Carlos->update("Carlinhos");
+
+//Testando a classe Aventureiro
+$Carlos = new Aventureiro("Carlinhos", 4, 100);
 
 
-#$Carlos->insert(); 
-#$aventureiroSelecionado = $Carlos->select();
-#echo $aventureiroSelecionado;
+//Cria um array de arrays, averigurando aqui a possibilidade de encodar o resultado em json pra usar de API
+$aventureiroSelecionado = $Carlos->select();
+
+#var_dump($aventureiroSelecionado);
+var_dump($aventureiroSelecionado[0]['id']);
+
+$Carlos->delete();
